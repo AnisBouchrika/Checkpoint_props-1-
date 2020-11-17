3ce60a9fac9aa638f60d5889eff316a195defeb5
AnisBouchrika <anice.bouchrika@gmail.com>
2020-11-17 12:44:24 +0100

    last commit

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -9,11 +9,11 @@ const profile = (props) =>{
         return (
         <div>
       
-
+                <button onClick={props.alert}>Click me</button>
                 <h1> Full name :{props.fullName}     </h1>
                 <h1> Bio :      {props.bio}          </h1>
                 <h1> Profession :{props.profession}   </h1>
-                <button onClick={props.alert}>Click me</button>
+                
                 {props.children}
                 
         </div>
